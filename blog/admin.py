01902cefe6c732806c08dc73c1bbd42c2b0e06e1
Mahan Mazaheri <mahan78ir@gmail.com>
2024-07-29 10:45:19 +0330

    add requirements.txt and custom admin model

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user' , 'avatar' , 'description']
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title' , 'content']
+    list_display = ['title' , 'category' , 'created_at']
+
+admin.site.register(Article , ArticleAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title' , 'cover']
+
+admin.site.register(Category,CategoryAdmin )
