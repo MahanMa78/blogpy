c6e5fcf8f2fdc35f88477d21ac6bedf697a542f4
Mahan Mazaheri <mahan78ir@gmail.com>
2024-07-29 09:28:18 +0330

    add conceptual modeling for aritcle and category and user profile

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField #bayad yek python manage.py collectstatic ham bezanim ta yekseri file ro baraye ma tanzim kone
+from datetime import datetime
 
-# Create your models here.
+# elat inke ma az FileField be jaye ImageFild estefade kardim in bood ke FileField khali kamel tar hast
+# ama ImageFiled dige niazi be validation nadare vali dar FileField bayad taarif konim
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1] 
+    valid_extensions = ['.jpg' , '.png' ]
+    # dar 2 khat bala dar ext miam va esmme on file ro tike tike mikonim ta baadan on ro check konim [1] baraye enethaye file mahsob mishe
+    #va dar khat baadish miam va format hayee ke mikhahim ro dakhelesh minevisim
+    if not ext.lower() in valid_extensions:
+        raise ValidationError("Unsupported file extension.")
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar/' , null= False , blank=False , validators=[validate_file_extension])
+    description = models.CharField(max_length=512 , null=False , blank= False)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=128 , null=False , blank=False)
+    cover = models.FileField(upload_to='files/article_cover/' , null=False , blank= False , validators=[validate_file_extension])
+    content = RichTextField()
+    created_at = models.DateTimeField(default=datetime.now , blank= False)
+    category = models.ForeignKey("Category" , on_delete=models.CASCADE)
+    # علت اینکه Category رو داخل ' ' گذاشتیم فریب جنگو هست
+    author = models.OneToOneField(UserProfile , on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=128 , null= False, blank=False)
+    cover = models.FileField(upload_to='files/category_cover/',null= True , blank= True , validators=[validate_file_extension])
+
+
+    def __str__(self) :
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for config project.
 
@@ -37,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'blog',
+    'ckeditor',
+
 
 
 ]
@@ -77,10 +79,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogpy',
+        'HOST':'localhost',
+        'USER' : 'mahanmazaheri',
+        'PASSWORD' : '0022737944',
+        'PORT' : '5432',
     }
 }
 
@@ -119,9 +131,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#CKEditor Settings 
+#Static Files
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = 'static/'
+
+
+# CKEditor 
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
