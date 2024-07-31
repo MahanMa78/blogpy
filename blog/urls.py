ddf76ee10eca9849f84df3e15ab0f84ed9d1ed71
Mahan Mazaheri <mahan78ir@gmail.com>
2024-07-31 12:45:36 +0330

    fix a bug

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('article/',views.SingleArticleAPIView.as_view(),name='single_article'),
     path('article/search/',views.SearchArticleAPIView.as_view(),name='search_article'),
     path('article/submit/',views.SubmitArticleAPIView.as_view(),name='submit_article'),
-    path('article/update_cover/',views.UpdateArticleAPIView.as_view(),name='update_article'),
+    path('article/update-cover/',views.UpdateArticleAPIView.as_view(),name='update_article'),
 ]
