eeeb5deb7e0595e92a3daff72775911a8f6cac62
Mahan Mazaheri <mahan78ir@gmail.com>
2024-08-02 19:24:36 +0330

    create accounts/forms.py and update accounts/admin and blog/admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomChangeForm,CustomUserCreationForm
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomChangeForm
+    list_display = ['username','email','is_staff']
+    
+
+# Register your models here.
+admin.site.register(CustomUser,CustomUserAdmin)
